673b89087ad190524e35a1e7599b14ad2e95ae2e
VladimirNK <nk.kozlov@gmail.com>
2022-09-15 14:19:35 +0300

    App Interface prepare

---
+++ GCD swiftbook/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  GCD swiftbook
+//
+//  Created by User on 15.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
