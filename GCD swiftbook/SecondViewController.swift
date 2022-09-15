40a2d8c3f5880006b5b24d47babb3c383ff00531
VladimirNK <nk.kozlov@gmail.com>
2022-09-15 14:48:07 +0300

    Add loading image function and activity indicator

--- GCD swiftbook/SecondViewController.swift
+++ GCD swiftbook/SecondViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
+        
+    }
 
 }
