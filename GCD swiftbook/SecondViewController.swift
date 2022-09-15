44c4f57de6f23d199e1f59ca81cceedb88c6ad3a
VladimirNK <nk.kozlov@gmail.com>
2022-09-15 15:19:10 +0300

    Applying GCD

--- GCD swiftbook/SecondViewController.swift
+++ GCD swiftbook/SecondViewController.swift
@@ -34,9 +34,13 @@ class SecondViewController: UIViewController {
         imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
-        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
-        self.image = UIImage(data: imageData)
-        
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
     }
 
 }
